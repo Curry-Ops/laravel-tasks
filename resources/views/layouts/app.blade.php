ff408d8578d2e321dcd739e318559ec0d901525f
Curry-Ops <wheldon79@gmail.com>
2025-12-05 17:36:51 -0500

    Change title and update comments in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,28 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Quickstart - Basic</title>
+    <title>Task List</title>
 
     <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- Bootstrap -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
-    {{-- Original Elixir CSS reference (no longer used, kept for reference)
-    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
-    --}}
-
-    {{-- Custom app CSS in public/css/app.css --}}
-    <link href="{{ asset('css/app.css') }}?v=2" rel="stylesheet">
+    <!-- Custom App CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body id="app-layout">
+
     <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
-                <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
                     Task List
                 </a>
@@ -34,12 +30,11 @@
         </div>
     </nav>
 
-    {{-- Page content from child views (e.g., tasks.blade.php) --}}
+    <!-- Page Content -->
     @yield('content')
 
-    <!-- JavaScripts -->
+    <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
