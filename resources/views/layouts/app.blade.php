0df8dbf9a12d790ed99ef48cb58738fb6df10020
Curry-Ops <wheldon79@gmail.com>
2025-12-05 13:47:52 -0500

    Add link to custom app.css stylesheet
    link added to update background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="/css/app.css">
+
 
     <style>
         body {
